b743f6c0b2abac14e916fc8d299b8449efbf5b0b
Azdoud1 <azdoudmohamed1999@gmail.com>
2023-03-25 12:29:29 +0100

    spacey

--- src/app/pages/redevable/redevable-creat/redevable-creat.component.ts
+++ src/app/pages/redevable/redevable-creat/redevable-creat.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {RedevableService} from "../../../controller/service/redevable.service";
+import {Redevable} from "../../../controller/model/redevable.model";
 
 @Component({
   selector: 'app-redevable-creat',
   templateUrl: './redevable-creat.component.html',
   styleUrls: ['./redevable-creat.component.css']
 })
-export class RedevableCreatComponent {
+export class RedevableCreatComponent implements OnInit{
+
+  constructor(private _redevabelservice:RedevableService) {
+  }
+
+  ngOnInit(): void {
+  }
+  public save(): void {
+    this._redevabelservice.save();
+  }
+
+
+  get redevable(): Redevable {
+
+    return this._redevabelservice.redevable;
+  }
+
+  set redevable(value: Redevable) {
+    this._redevabelservice.redevable = value;
+  }
+
+  get redevables(): Array<Redevable> {
+    return this._redevabelservice.redevables;
+  }
+
+  set redevables(value: Array<Redevable>) {
+    this._redevabelservice.redevables = value;
+  }
 
 }
